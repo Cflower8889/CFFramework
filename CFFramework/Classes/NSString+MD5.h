dab14a7eb8d3a246158351aede3628ab165b4402
chenyongwei <chenyongwei@syswin.com>
2018-06-08 18:19:57 +0800

    添加字符串分类

---
+++ CFFramework/Classes/NSString+MD5.h
@@ -0,0 +1,12 @@
+//
+//  NSString+MD5.h
+//  CFFramework
+//
+//  Created by chen on 2018/6/8.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+@end
---
+++ CFFramework/Classes/NSString+MD5.m
@@ -0,0 +1,12 @@
+//
+//  NSString+MD5.m
+//  CFFramework
+//
+//  Created by chen on 2018/6/8.
+//
+
+#import "NSString+MD5.h"
+
+@implementation NSString (MD5)
+
+@end
