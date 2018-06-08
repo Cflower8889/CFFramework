302a42a8706c081602b1c82d6600e6aea6476d92
chenyongwei <chenyongwei@syswin.com>
2018-06-08 18:01:26 +0800

    添加view 分类

---
+++ CFFramework/Classes/UIView+Frame.h
@@ -0,0 +1,12 @@
+//
+//  UIView+Frame.h
+//  CFFramework
+//
+//  Created by chen on 2018/6/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@end
---
+++ CFFramework/Classes/UIView+Frame.m
@@ -0,0 +1,12 @@
+//
+//  UIView+Frame.m
+//  CFFramework
+//
+//  Created by chen on 2018/6/8.
+//
+
+#import "UIView+Frame.h"
+
+@implementation UIView (Frame)
+
+@end
